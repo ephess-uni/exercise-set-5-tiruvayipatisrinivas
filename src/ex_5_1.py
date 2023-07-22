34c4ce95c8a6063195723397bddd9cc66a84ddc8
tiruvayipatisrinivas <136854399+tiruvayipatisrinivas@users.noreply.github.com>
2023-07-22 22:04:09 +0530

    Update ex_5_1.py

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -3,6 +3,7 @@ try:
     from src.ex_5_0 import line_count
 except ImportError:
     from ex_5_0 import line_count
+import argparse
 
 
 def main(infile):
@@ -22,3 +23,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.infile:
         main(args.infile)
+
