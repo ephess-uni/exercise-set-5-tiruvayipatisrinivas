d08150b7af0a129d1e36341fee125d59106679cc
tiruvayipatisrinivas <136854399+tiruvayipatisrinivas@users.noreply.github.com>
2023-07-22 22:19:12 +0530

    Update ex_5_4.py

--- src/ex_5_4.py
+++ src/ex_5_4.py
@@ -15,6 +15,9 @@ data_dir = root_dir / "data"
 output_dir = root_dir / "outputs"
 input_file = data_dir / "ex_5_4-data.csv"
 output_file = output_dir / "ex_5_4-processed.csv"
+raw_data = np.loadtxt(input_file)
+raw_data[raw_data<0] = 0
+np.savetxt(output_file, raw_data)
 
 # Process the input data using numpy
 
