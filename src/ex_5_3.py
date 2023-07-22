57ac43131cdd692ce620b86de173cbc5e11bc728
tiruvayipatisrinivas <136854399+tiruvayipatisrinivas@users.noreply.github.com>
2023-07-22 22:11:46 +0530

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -7,6 +7,7 @@ This module contains an entry point that:
 """
 import numpy as np
 from argparse import ArgumentParser
+import os
 
 if __name__ == "__main__":
     # Create your argument parser object here.
@@ -16,4 +17,14 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+        
+    parser = ArgumentParser(description='This program applies a standard scale transform to the data in infile and writes it to outfile.')
+    parser.add_argument('infile',help='provide input file path'')
+    parser.add_argument('outfile',help='provide output file path')
+    args = parser.parse_args()
+    raw_data = np.loadtxt(args.infile)
+    normed = (raw_data - raw_data.mean(axis=0)) / raw_data.std(axis=0)
+    processed = normed
+    root_dir = get_repository_root()
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    np.savetxt(args.outfile, processed, fmt='%.2e')
